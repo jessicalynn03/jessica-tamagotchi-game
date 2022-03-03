d04fd63f602b3c94de587f4ad09a58bb0265f212
jessicalynn03 <jessica.barthelemy03@gmail.com>
2022-03-03 18:38:34 -0500

    brainstorming part 3: Javascript

--- js/main.js
+++ js/main.js
@@ -1,10 +1,13 @@
-console.log("hello")
+const timeH1 = document.querySelector("#timer")
+const hungerEl = document.querySelector(".hunger")
+const tiredEl = document.querySelector(".tired")
+const boredEl = document.querySelector(".bored")
+const startButton = document.querySelector("#start")
+const restartButton = document.querySelector("#restart")
 
+const feedPet = document.querySelector("#feed")
+const playWithPet = document.querySelector("#play")
+const sleepNow = document.querySelector("#sleep")
 
-
-let name = document.querySelector("span")
-
-console.log(name)
-
-name.innerText = "";
-
+let time = 5; 
+let name = ""
