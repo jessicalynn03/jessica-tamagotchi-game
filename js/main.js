01fbfa875ffab415d0a5f7a5cba5247931d9778e
jessicalynn03 <jessica.barthelemy03@gmail.com>
2022-03-03 18:53:11 -0500

    js brainstorming part 3

--- js/main.js
+++ js/main.js
@@ -4,6 +4,7 @@ const tiredEl = document.querySelector(".tired")
 const boredEl = document.querySelector(".bored")
 const startButton = document.querySelector("#start")
 const restartButton = document.querySelector("#restart")
+const yourName = document.querySelector("#yourName")
 
 const feedPet = document.querySelector("#feed")
 const playWithPet = document.querySelector("#play")
@@ -11,3 +12,22 @@ const sleepNow = document.querySelector("#sleep")
 
 let time = 5; 
 let name = ""
+
+
+class Tamagotchipet{
+    constructor(){
+        this.name = "";
+        this.hunger = hunger;
+        this.tired = tired;
+        this.bored = bored;
+        this.age = age;
+    }
+}
+
+class Tamagotchipet extends Data{
+    constructor(){
+        this.feed = feed;
+        this.play = play;
+        this.sleep = sleep;
+    }
+}
