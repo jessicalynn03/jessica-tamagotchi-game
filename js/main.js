b08a5369ce4556c9d0fe7e790d9f674a76d81265
jessicalynn03 <jessica.barthelemy03@gmail.com>
2022-03-04 11:02:26 -0500

    javascript brainstorming part 4

--- js/main.js
+++ js/main.js
@@ -6,28 +6,73 @@ const startButton = document.querySelector("#start")
 const restartButton = document.querySelector("#restart")
 const yourName = document.querySelector("#yourName")
 
-const feedPet = document.querySelector("#feed")
-const playWithPet = document.querySelector("#play")
-const sleepNow = document.querySelector("#sleep")
+
+const feedBtn = document.querySelector("#feed")
+const playBtn = document.querySelector("#play")
+const sleepBtn = document.querySelector("#sleep")
 
 let time = 5; 
 let name = ""
 
+let hungerNow = 0;
+let playNow = 0;
+let sleepNow = 0;
 
-class Tamagotchipet{
-    constructor(){
-        this.name = "";
-        this.hunger = hunger;
-        this.tired = tired;
-        this.bored = bored;
-        this.age = age;
-    }
+const addFood = () => {
+    hungerNow++;
+    hungerEl.innerText = hungerNow;
+};
+
+const addPlay = () => {
+    playNow++
+    boredEl.innerText = playNow; 
+};
+
+const addSleep = () => {
+    sleepNow++
+    tiredEl.innerText = sleepNow;
+};
+
+
+const decreaseTime = () => {
+    time--
+    timeH1.innerText = time;
 }
 
-class Tamagotchipet extends Data{
-    constructor(){
-        this.feed = feed;
-        this.play = play;
-        this.sleep = sleep;
-    }
-}
+
+
+
+
+
+
+
+startButton.addEventListener("click", decreaseTime)
+feedBtn.addEventListener("click", addFood);
+playBtn.addEventListener("click", addPlay);
+sleepBtn.addEventListener("click", addSleep);
+
+
+
+
+
+
+
+
+
+// class Tamagotchipet{
+//     constructor(){
+//         this.name = "";
+//         this.hunger = hunger;
+//         this.tired = tired;
+//         this.bored = bored;
+//         this.age = age;
+//     }
+// }
+
+// class Tamagotchipet extends Data{
+//     constructor(){
+//         this.feed = feed;
+//         this.play = play;
+//         this.sleep = sleep;
+//     }
+// }
