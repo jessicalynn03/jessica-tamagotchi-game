6efba11967c561f190ac1ee49e8346231200d9ef
jessicalynn03 <jessica.barthelemy03@gmail.com>
2022-03-06 20:12:39 -0500

    buttons have no functioniality

--- js/main.js
+++ js/main.js
@@ -1,12 +1,14 @@
 const countdownEl = document.querySelector("#countdown")
-const hungerEl = document.querySelector(".hunger")
-const tiredEl = document.querySelector(".tired")
-const boredEl = document.querySelector(".bored")
+let hungerEl = document.getElementById("hunger")
+let tiredEl = document.getElementById("tired")
+let boredEl = document.getElementById("bored")
+
 const startButton = document.querySelector("#start")
 const restartButton = document.querySelector("#restart")
+const pauseButton = document.querySelector("#pause")
 const yourName = document.querySelector("#yourName")
 const ageEl = document.querySelector(".age")
-
+const imageEl = document.querySelector("myImage")
 
 const feedBtn = document.querySelector("#feed")
 const playBtn = document.querySelector("#play")
@@ -15,7 +17,7 @@ const sleepBtn = document.querySelector("#sleep")
 
 let startingMinutes = 5;
 let time = startingMinutes * 60;
-const minutes = math.floor(time / 60);
+const minutes = Math.floor(time / 60);
 const seconds = time % 60;
 let name = ""
 
@@ -49,48 +51,106 @@ const subSleep = () => {
     
 };
 
-let hungerInterval = setInterval(function(){
-    hungerNow++
-    hungerEl.innerText = "Hungry: " + hungerNow;
-}, 2000)
+const ageCount = () =>{
+    let ageInterval = setInterval(function(){
+        age++
+        ageEl.innerText = "Age: " + age;
+        }, 20000)
+}
 
-let playInterval = setInterval(function(){
+const hungerCount = () =>{
+    let hungerInterval = setInterval(function(){
+        hungerNow++
+        hungerEl.innerText = "Hungry: " + hungerNow;
+    }, 2000)
+}
+
+const playCount = () =>{
+    let playInterval = setInterval(function(){
     playNow++ 
     boredEl.innerText = "Bored: " + playNow;
 }, 3000)
-let sleepInterval = setInterval(function(){
-    sleepNow++
-    tiredEl.innerText = "Tired: " + sleepNow;
-}, 4000)
-
-
-let ageInterval = setInterval(function(){
-age++
-ageEl.innerText = "Age: " + age;
-}, 20000)
-
-
-
-const updateCountdown = () =>{
-    time--
-    countdownEl.innerText = `${minutes}: ${seconds}`;
 }
 
+const sleepCount = () =>{
+    let sleepInterval = setInterval(function(){
+        sleepNow++
+        tiredEl.innerText = "Tired: " + sleepNow;
+    }, 4000)
+    
+}
+function updateCountdown(){
+    const minutes = Math.floor(time / 60);
+    let seconds = time % 60;
+    seconds = seconds < 5 ? "0" * seconds : seconds;
+    countdownEl.innerText = `${minutes}: ${seconds}`;
+    time--;
+}
+setInterval(updateCountdown, 1000)
+
+
+const startCount = () =>{
+    sleepCount();
+    ageCount();
+    playCount();
+    hungerCount();
+}
+
+const removePng = () =>{
+    const imageEl = document.querySelector("myImage");
+    imageEl.remove();
+};
+
+const addFeedPng = () =>{
+    removePng();
+    let feedPng = document.createElement("img");
+    feedPng.setAttribute("src", "feed.png");
+    document.querySelector("#myImage").append(feedPng);
+};
+
+const addPlayPng = () =>{
+    removePng();
+    let playPng = document.createElement("img");
+    playPng.setAttribute("src", "cat.png");
+    document.querySelector("myImage").append(playPng);
+};
+
+const addSleepPng = () =>{
+    removePng();
+    let sleepPng = document.createElement("img");
+    sleepPng.setAttribute("src", "sleepcat.gif");
+    document.querySelector("myImage").append(sleepPng);
+};
 
 
 
 
-// function updateCountdown(){
-//     const minutes = math.floor(time / 60);
-//     let seconds = time % 60;
-//     seconds = seconds < 5 ? "0" * seconds : seconds;
-//     countdownEl.innerText = `${minutes}: ${seconds}`;
-//     time--;
-// }
-// setInterval(updateCountdown, 1000)
 
 
-startButton.addEventListener("click", updateCountdown)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+startButton.addEventListener("click", startCount);
        
 
 
